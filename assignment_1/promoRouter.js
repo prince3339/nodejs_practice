26d44a679b9ae0c8f529945ae2dd293c86771ca4
Sabbir Ahmed <Sabbir Ahmed>
2017-02-10 23:25:57 +0600

    ass 1 fixes

---
+++ assignment_1/promoRouter.js
@@ -0,0 +1,47 @@
+module.exports = function () {
+  var express = require('express'),
+      bodyParser = require('body-parser');
+
+  var promoRouter = express.Router();
+
+  promoRouter.use(bodyParser.json());
+
+  promoRouter.route('/')
+   .all(function(req, res, next) {
+      res.writeHead(200, {'content-type': 'text/plain'});
+      next();
+   })
+   .get(function(req, res, next) {
+      res.end('Will send all the promotions to you!');
+   })
+   .post(function(req, res, next){
+       res.end('Will add the promotion: ' + req.body.name + ' with details: ' + req.body.description);
+   })
+
+   .delete(function(req, res, next){
+       res.end('Deleting all promotions');
+   });
+
+   promoRouter.route('/:promoID')
+     .all(function(req,res,next) {
+           res.writeHead(200, { 'Content-Type': 'text/plain' });
+           next();
+     })
+
+     .get(function(req,res,next){
+             res.end('Will send details of the promotion: ' + req.params.promoID +' to you!');
+     })
+
+     .put(function(req, res, next){
+         res.write('Updating the promotion: ' + req.params.promoID + '\n');
+         res.end('Will update the promotion: ' + req.body.name +
+                 ' with details: ' + req.body.description);
+     })
+
+     .delete(function(req, res, next){
+             res.end('Deleting promotion: ' + req.params.promoID);
+     });
+
+
+  return promoRouter;
+}
---
+++ assignment_1/server.js
@@ -0,0 +1,25 @@
+var dishRouter = require('./dishRouter');
+var promoRouter = require('./promoRouter');
+var leaderRouter = require('./leaderRouter');
+
+var express = require('express');
+var morgan = require('morgan');
+
+var hostname = 'localhost';
+var port = 3000;
+
+var app = express();
+
+app.use(morgan('dev'));
+
+app.use('/dishes', dishRouter());
+app.use('/promotions', promoRouter());
+app.use('/leadership', leaderRouter());
+//app.use('/leadership', leaderRouter());
+
+//app.use(express.static(__dirname + '/public'));
+
+app.listen(port, hostname, function(){
+  console.log(`Server running at http://${hostname}:${port}/`);
+  //console.log(dishRouter());
+});
