52d625a9db5ee5c3e9dfb224c8d52b55f781f5bf
Prince <prince3339@ymail.com>
2017-03-23 14:49:06 +0600

    used mongoose population

--- assignment_3/routes/users.js
+++ assignment_3/routes/users.js
@@ -14,12 +14,20 @@ router.get('/', Verify.verifyOrdinaryUser, Verify.verifyAdmin, function(req, res
 
 router.post('/register', function(req, res) {
     User.register(new User({ username : req.body.username }),
-      req.body.password, function(err, user) {
+        req.body.password, function(err, user) {
         if (err) {
             return res.status(500).json({err: err});
         }
+        if(req.body.firstname) {
+            user.firstname = req.body.firstname;
+        }
+        if(req.body.lastname) {
+            user.lastname = req.body.lastname;
+        }
+        user.save(function(err,user) {
         passport.authenticate('local')(req, res, function () {
             return res.status(200).json({status: 'Registration Successful!'});
+        });
         });
     });
 });
@@ -41,7 +49,7 @@ router.post('/login', function(req, res, next) {
         });
       }
 
-      var token = Verify.getToken(user);
+        var token = Verify.getToken(user);
         res.status(200).json({
         status: 'Login successful!',
         success: true,
